f95c2d29f0767daff679145fffc9f40b83588e61
Cassie <cassgrey05@gmail.com>
2026-03-20 18:41:24 -0700

    initial commit

---
+++ macros/target/debug/deps/framework_derive_macros-8019333744742ba2.d
@@ -0,0 +1,5 @@
+/home/deck/projects/reliquary/macros/target/debug/deps/framework_derive_macros-8019333744742ba2.d: src/lib.rs
+
+/home/deck/projects/reliquary/macros/target/debug/deps/libframework_derive_macros-8019333744742ba2.so: src/lib.rs
+
+src/lib.rs:
---
+++ macros/target/debug/libframework_derive_macros.d
@@ -0,0 +1,1 @@
+/home/deck/projects/reliquary/macros/target/debug/libframework_derive_macros.so: /home/deck/projects/reliquary/macros/src/lib.rs

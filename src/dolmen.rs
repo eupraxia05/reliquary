bbe3dd753c4c598ad57c44888ce9eb2ac8e4989e
Cassie <cassgrey05@gmail.com>
2026-05-06 15:41:49 -0700

    added DbSource

--- src/dolmen.rs
+++ src/dolmen.rs
@@ -1,7 +1,8 @@
 use crate::Error;
 use crate::db::DbConnection;
 use dolmen::prelude::*;
-use std::path::PathBuf;
+use std::ffi::OsString;
+use std::path::{Path, PathBuf};
 
 impl From<dolmen::Error> for Error {
     fn from(e: dolmen::Error) -> Self {
@@ -28,18 +29,30 @@ impl Plugin for DbPlugin {
     }
 
     fn startup(context: &mut Context) -> dolmen::Result<()> {
-        println!("asdf");
-        let db_connection = if context
+        let config = context
             .get_resource::<DbConfig>()
-            .ok_or(dolmen::Error::default())?
-            .open_db_in_memory
-        {
-            DbConnectionRes::open_test(context)
-                .map_err(|e| dolmen::Error::new(e.message.unwrap_or_default()))
-        } else {
-            DbConnectionRes::open_default(context)
-                .map_err(|e| dolmen::Error::new(e.message.unwrap_or_default()))
-        }?;
+            .cloned()
+            .unwrap_or_default();
+
+        let db_connection = match config.source {
+            DbSource::InMemory => DbConnectionRes::open_in_memory(context)
+                .map_err(|e| dolmen::Error::new(e.message.unwrap_or_default()))?,
+            DbSource::CacheDir {
+                application_name,
+                filename,
+            } => {
+                let dirs = directories::ProjectDirs::from("", "", application_name)
+                    // translate to an error if it failed
+                    .ok_or(Error::new("Failed to get data directory"))?;
+
+                DbConnectionRes::open_from_path(context, &dirs.data_dir().join(filename))
+                    .map_err(|e| dolmen::Error::new(e.message.unwrap_or_default()))?
+            }
+            DbSource::CustomPath { path } => {
+                DbConnectionRes::open_from_path(context, &PathBuf::from(path))
+                    .map_err(|e| dolmen::Error::new(e.message.unwrap_or_default()))?
+            }
+        };
 
         context.add_resource(db_connection);
 
@@ -51,31 +64,17 @@ impl Plugin for DbPlugin {
 struct DbConnectionRes(crate::db::DbConnection);
 
 impl DbConnectionRes {
-    fn get_default_db_path() -> crate::Result<PathBuf> {
-        // get the cache directory for this
-        // application
-        let dirs = directories::ProjectDirs::from("", "", "training_assistant")
-            // translate to an error if it failed
-            .ok_or(Error::new("Failed to get data directory"))?;
-
-        Ok(dirs.data_dir().join("data/data.db"))
-    }
-
-    // opens a db connection at the default db path
-    #[deprecated(note = "training_assistant specific, should be moved there")]
-    pub(crate) fn open_default(context: &Context) -> crate::Result<Self> {
-        assert!(!cfg!(test));
-        let db_path = Self::get_default_db_path()?;
+    // opens a db connection at the given db path
+    pub(crate) fn open_from_path(context: &Context, path: &PathBuf) -> crate::Result<Self> {
         let table_configs = match context.get_resource::<DbTableConfigsRes>() {
             Some(t) => t.0.clone(),
             None => Vec::new(),
         };
-        Ok(Self(DbConnection::open_from_path(&db_path, table_configs)?))
+        Ok(Self(DbConnection::open_from_path(&path, table_configs)?))
     }
 
-    // opens a db connection at a test db path
-    #[deprecated(note = "training_assistant specific, should be moved there")]
-    pub(crate) fn open_test(context: &Context) -> crate::Result<Self> {
+    // opens a db connection in memory
+    pub(crate) fn open_in_memory(context: &Context) -> crate::Result<Self> {
         let table_configs = match context.get_resource::<DbTableConfigsRes>() {
             Some(t) => t.0.clone(),
             None => Vec::new(),
@@ -128,14 +127,27 @@ impl DbContextExt for Context {
     }
 }
 
-#[derive(Resource, Default)]
+#[derive(Default, Clone)]
+pub enum DbSource {
+    #[default]
+    InMemory,
+    CacheDir {
+        application_name: &'static str,
+        filename: OsString,
+    },
+    CustomPath {
+        path: PathBuf,
+    },
+}
+
+#[derive(Resource, Default, Clone)]
 pub struct DbConfig {
-    pub open_db_in_memory: bool,
+    source: DbSource,
 }
 
 #[cfg(test)]
 mod test {
-    use crate::dolmen::{DbConfig, DbContextExt, DbPlugin};
+    use crate::dolmen::{DbConfig, DbContextExt, DbPlugin, DbSource};
     use crate::prelude::*;
     use crate::{Error, Result};
     use dolmen::prelude::*;
@@ -157,6 +169,57 @@ mod test {
     }
 
     #[test]
+    fn db_custom_path_test() -> Result<()> {
+        let directories = directories::ProjectDirs::from("", "", "reliquary_test").unwrap();
+        let db_path = directories.data_dir().join("data.db");
+        if std::fs::exists(db_path.clone()).unwrap() {
+            std::fs::remove_file(db_path.clone()).unwrap();
+        }
+
+        let mut context = Context::new();
+        context.add_plugin(TestPlugin)?;
+        context.add_plugin(DbPlugin)?;
+        context
+            .get_resource_mut::<DbConfig>()
+            .ok_or(Error::default())?
+            .source = DbSource::CustomPath {
+            path: db_path.clone(),
+        };
+
+        context.startup()?;
+
+        assert!(std::fs::exists(db_path).unwrap());
+
+        Ok(())
+    }
+
+    #[test]
+    fn db_cache_dir_test() -> Result<()> {
+        let directories = directories::ProjectDirs::from("", "", "reliquary_test").unwrap();
+        let db_path = directories.data_dir().join("data.db");
+        if std::fs::exists(db_path.clone()).expect("couldn't check if db exists") {
+            std::fs::remove_file(db_path.clone()).expect("couldn't remove db");
+        }
+
+        let mut context = Context::new();
+        context.add_plugin(TestPlugin)?;
+        context.add_plugin(DbPlugin)?;
+        context
+            .get_resource_mut::<DbConfig>()
+            .ok_or(Error::default())?
+            .source = DbSource::CacheDir {
+            application_name: "reliquary_test",
+            filename: "data.db".into(),
+        };
+
+        context.startup()?;
+
+        assert!(std::fs::exists(db_path).unwrap());
+
+        Ok(())
+    }
+
+    #[test]
     fn db_table_ops_test() -> Result<()> {
         // create a context and add our test plugin
         let mut context = Context::new();
@@ -165,7 +228,7 @@ mod test {
         context
             .get_resource_mut::<DbConfig>()
             .ok_or(Error::default())?
-            .open_db_in_memory = true;
+            .source = DbSource::InMemory;
 
         context.startup()?;
 
